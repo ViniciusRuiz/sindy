0d79184b5adaf883b3764214e1229d0024f1113b
Kentaro Fujiy <kentaro@manacadigital.com.br>
2020-06-15 03:54:18 -0300

    added sobre

--- app.js
+++ app.js
@@ -1,43 +1,18 @@
 
-var app = angular.module("siteSindy", []);
-app.controller("orcamentoCtrl", function ($scope, $http) {
-	$scope.formData = {};
-	$scope.processForm = function () {
+var app = angular.module("siteSindy", ["ngRoute"]);
+app.config(function($routeProvider) {
+	$routeProvider
+	  .when("/", {
+		templateUrl: "pages/home.html"
+	  })
+	  .when("/sobre", {
+		templateUrl: "pages/sobre.html"
+	  })
+	  .when("/three", {
+		templateUrl: "three.html"
+	  })
+	  .when("/four", {
+		templateUrl: "four.html"
+	  });
+  });
 
-		console.dir($scope.formData);
-		$http({
-			method: "POST",
-			url: "https://sotreqlab.herokuapp.com/familiaminis.json",
-			data: $.param($scope.formData),
-			headers: { "Content-Type": "application/x-www-form-urlencoded"
-			} }).then(function (){
-                window.location = "enviado.html";
-            });
-		$scope.formData = {};
-		$scope.orcamentoForm.$setPristine();
-        $scope.orcamentoForm.$setUntouched();
-
-			};
-
-})
-app.controller("orcamentoRodapeCtrl", function ($scope, $http) {
-	$scope.formData = {};
-	$scope.processrodapeForm = function () {
-
-		console.dir($scope.formData);
-		$http({
-			method: "POST",
-			url: "https://sotreqlab.herokuapp.com/familiaminis.json",
-			data: $.param($scope.formData),
-			headers: { "Content-Type": "application/x-www-form-urlencoded"
-			} }).then(function (){
-                window.location = "enviado.html";
-            });
-		$scope.formData = {};
-		$scope.orcamentorodapeForm.$setPristine();
-        $scope.orcamentorodapeForm.$setUntouched();
-
-			};
-
-}
-);
